370e3b63222554e51b75b3b5baa29096f6dfcb0d
Octol1ttle <l1ttleofficial@outlook.com>
2023-01-26 23:31:23 +0500

    Initial commit
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

---
+++ src/main/java/ru/octol1ttle/damagevignette/client/DamageVignette.java
@@ -0,0 +1,12 @@
+package ru.octol1ttle.damagevignette.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class DamageVignette implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
