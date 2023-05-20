f2a705c826a11aa8078d36f08251bc7348c756aa
Octol1ttle <l1ttleofficial@outlook.com>
2023-05-20 20:22:29 +0500

    Do not show Damage Vignette if the camera entity is in creative/spectator mode
    Closes #10
    
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- common/src/main/java/ru/octol1ttle/damagevignette/DamageVignette.java
+++ common/src/main/java/ru/octol1ttle/damagevignette/DamageVignette.java
@@ -3,6 +3,7 @@ package ru.octol1ttle.damagevignette;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 
 public class DamageVignette {
     public static final String MOD_ID = "damagevignette";
@@ -11,6 +12,12 @@ public class DamageVignette {
 
     public static boolean shouldRenderVignette(Minecraft minecraft, boolean vignetteAllowed) {
         LivingEntity entity = (LivingEntity) minecraft.cameraEntity;
+        if (entity instanceof Player player) {
+            if (player.isCreative() || player.isSpectator()) {
+                DamageVignette.curOpacity = 0.0f;
+                return vignetteAllowed;
+            }
+        }
         //noinspection DataFlowIssue
         float hpScaled = entity.getHealth() / entity.getMaxHealth() * 100.0f;
         float confLow = Mth.clamp(DamageVignette.configProvider.getLowThreshold(), 0.0f, DamageVignette.configProvider.getHighThreshold());
